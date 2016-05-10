5819e9e18bc196a37c9a49a7932b9ebdff046e43
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2016-05-11 01:03:14 +0400

    Solved 1904.

--- gvzhf/p19/p1904_maybe.cpp
+++ gvzhf/p19/p1904_maybe.cpp
@@ -12,18 +12,34 @@ int main() {
         cin >> a[n - 1 - i];
     }
     int map[2 * SZ] = {};
-    map[SZ] = 1;
+    map[SZ+0] = 1;
+    map[SZ+1] = 1;
     for (int i = 0; i < n; i++) {
-        for (int j = -SZ + 1009; j < SZ - 1009; j++) {
+        for (int j = 0; j < SZ - 1009; j++) {
             map[SZ + j + a[i]] |= (map[SZ + j] & (1 << i)) << 1;
-            map[SZ - j - 1 + a[i]] |= (map[SZ + j] & (1 << i)) << 1;
+            map[SZ - j + a[i]] |= (map[SZ + j] & (1 << i)) << 1;
         }
     }
     bool in = false;
+    int cnt = 0;
     for (int j = -SZ; j < SZ; j++) {
         if (in) {
             if (!(map[SZ + j] & (1 << n))) {
-                cout << j << endl;
+                in = false;
+                cnt++;
+            }
+        } else {
+            if (map[SZ + j] & (1 << n)) {
+                in = true;
+            }
+        }
+    }
+    cout << cnt << endl;
+    in = false;
+    for (int j = -SZ; j < SZ; j++) {
+        if (in) {
+            if (!(map[SZ + j] & (1 << n))) {
+                cout << j-1 << endl;
                 in = false;
             }
         } else {
