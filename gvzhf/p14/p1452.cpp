96f1a92f482ac3868dbac3a1bba5354e2d956fe8
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2016-06-10 02:44:46 +0400

    Solved: 1452.

--- gvzhf/p14/p1452.cpp
+++ gvzhf/p14/p1452.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <set>
 #include <map>
-#include <unordered_set>
-#include <unordered_map>
 
 using namespace std;
 
@@ -10,9 +7,8 @@ int main() {
     size_t n;
     cin >> n;
     map<int, int> inp;
-
     for (int i = 0; i < n; i++) {
-        int t;
+        unsigned int t;
         cin >> t;
         inp[t] = i + 1;
     }
@@ -24,39 +20,36 @@ int main() {
         cout << 2 << endl << inp.begin()->second << ' ' << inp.rbegin()->second;
         return 0;
     }
-    short int d[n][n] = {};
+    short int L[n][n] = {};
     int set[n];
-    unordered_map<int, int> pos;
-    {
-        int i = 0;
-        for (auto it = inp.begin(); it != inp.end(); ++it, i++) {
-            set[i] = it->first;
-            pos[it->first] = i;
-            d[i][n - 1] = d[n - 1][i] = 2;
-        }
+    short int max_cnt = 2;
+    auto it = inp.begin();
+    for (int i = 0; i < n; ++it, i++) {
+        set[i] = it->first;
+        L[i][n - 1] = 2;
     }
-    int max_cnt = 0, max_from = 0, max_step = 0;
-    bool need_recalc = true;
-    while (need_recalc) {
-        need_recalc = false;
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 2; j > i; j--) {
-                int k = 2 * set[j] - set[i];
-                if (pos.count(k)) {
-                    if (d[j][pos[k]]) {
-                        d[i][j] = d[j][pos[k]] + 1;
-                        if (d[i][j] > max_cnt) {
-                            max_cnt = d[i][j];
-                            max_from = set[i];
-                            max_step = set[j] - set[i];
-                        }
-                    } else {
-                        need_recalc = true;
-                    }
-                } else {
-                    d[i][j] = 2;
+    int max_from = set[0], max_step = set[1] - set[0];
+    for (int j = n - 2; j >= 1; j--) {
+        int i = j - 1, k = j + 1;
+        while (i >= 0 && k <= n - 1) {
+            if (set[i] + set[k] < 2 * set[j]) {
+                k++;
+            } else if (set[i] + set[k] > 2 * set[j]) {
+                L[i][j] = 2, i--;
+            } else {
+                L[i][j] = L[j][k] + 1;
+                if (max_cnt < L[i][j]) {
+                    max_cnt = L[i][j];
+                    max_from = set[i];
+                    max_step = set[j] - set[i];
                 }
+                i--;
+                k++;
             }
+        }
+        while (i >= 0) {
+            L[i][j] = 2;
+            i--;
         }
     }
     cout << max_cnt << endl;
