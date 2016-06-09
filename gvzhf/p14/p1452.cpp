30a9ef592c25eacb8d82da243ebaae61a9f792b5
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2016-06-10 01:37:55 +0400

    Wa: 1452.

--- gvzhf/p14/p1452.cpp
+++ gvzhf/p14/p1452.cpp
@@ -2,73 +2,66 @@
 #include <set>
 #include <map>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
-    int n;
-    map<int, int> num;
-    unordered_set<int> unum;
-    unordered_set<int> step3;
-    map<int, int> checked;
+    size_t n;
     cin >> n;
-    int max_num;
+    map<int, int> inp;
+
     for (int i = 0; i < n; i++) {
         int t;
         cin >> t;
-        max_num = max(max_num, t);
-        num[t] = i + 1;
-        unum.insert(t);
-        checked[t] = 0;
+        inp[t] = i + 1;
     }
-    if (num.size() == 1) {
+    n = inp.size();
+    if (n == 1) {
         cout << 1 << endl << 1;
         return 0;
-    }
-    for (auto it = num.begin(); it != num.end(); ++it) {
-        for (auto jt = it; ++jt != num.end();) {
-            if (it->first + 3 * (jt->first - it->first) > max_num) break;
-            if (unum.count(it->first + 3 * (jt->first - it->first)) && unum.count(it->first + 2 * (jt->first - it->first))) {
-                step3.insert(jt->first - it->first);
-            }
-        }
-    }
-    if (step3.size() == 0) {
-        for (auto it = num.begin(); it != num.end(); ++it) {
-            for (auto jt = it; ++jt != num.end();) {
-                if (unum.count(it->first + 2 * (jt->first - it->first))) {
-                    cout << 3 << endl;
-                    cout << num[it->first] << ' ' << num[jt->first] << ' ' << num[it->first + 2 * (jt->first - it->first)];
-                    return 0;
-                }
-            }
-        }
-        cout << 2 << endl << num.begin()->second << ' ' << num.rbegin()->second;
+    } else if (n == 2) {
+        cout << 2 << endl << inp.begin()->second << ' ' << inp.rbegin()->second;
         return 0;
     }
-    int max_cnt = 0, max_step, max_from;
-    for (auto it = step3.begin(); it != step3.end(); ++it) {
-        for (auto jt = num.begin(); jt != num.end(); ++jt) {
-            if (jt->first + *it * 3 > max_num) break;
-            if (checked[jt->first] != *it) {
-                int c = jt->first, cnt = 1;
-                while (unum.count(c + *it)) {
-                    c += *it;
-                    cnt++;
-                    checked[c] = *it;
+    short int d[n][n] = {};
+    int set[n];
+    unordered_map<int, int> pos;
+    {
+        int i = 0;
+        for (auto it = inp.begin(); it != inp.end(); ++it, i++) {
+            set[i] = it->first;
+            pos[it->first] = i;
+            d[i][n - 1] = d[n - 1][i] = 2;
+        }
+    }
+    int max_cnt = 0, max_from = 0, max_step = 0;
+    bool need_recalc = true;
+    while (need_recalc) {
+        need_recalc = false;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 2; j > i; j--) {
+                int k = 2 * set[j] - set[i];
+                if (pos.count(k)) {
+                    if (d[j][pos[k]]) {
+                        d[i][j] = d[j][pos[k]] + 1;
+                        if (d[i][j] > max_cnt) {
+                            max_cnt = d[i][j];
+                            max_from = set[i];
+                            max_step = set[j] - set[i];
+                        }
+                    } else {
+                        need_recalc = true;
+                    }
+                } else {
+                    d[i][j] = 2;
                 }
-                if (cnt > max_cnt) {
-                    max_cnt = cnt;
-                    max_step = *it;
-                    max_from = jt->first;
-                }
-                checked[jt->first] = *it;
             }
         }
     }
     cout << max_cnt << endl;
     for (int s = max_from, i = 0; i < max_cnt; i++, s += max_step) {
-        cout << num[s] << ' ';
+        cout << inp[s] << ' ';
     }
     return 0;
 }
